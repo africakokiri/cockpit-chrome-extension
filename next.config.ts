d9b805ba7dcd19637b6cc6eec1c500b02c0f626f
Choi Chan Hwan <africakokiri@gmail.com>
2025-03-14 11:25:56 +0900

    Next.js -> Vite, Delete all files

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export", // 정적 파일로 빌드
-  distDir: "out", // 빌드 결과 폴더,
-  assetPrefix: "/assets" // _next를 /assets로 변경
-};
-
-export default nextConfig;
