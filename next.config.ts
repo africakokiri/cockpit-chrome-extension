d3d5274ba5f6b11a4fcdbad4c4ba9b9e58bebb7e
Choi Chan Hwan <africakokiri@gmail.com>
2025-03-14 11:14:12 +0900

    Rename _next -> assets

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // 정적 파일로 빌드
-  distDir: "dist" // 빌드 결과 폴더
+  distDir: "out", // 빌드 결과 폴더,
+  assetPrefix: "/assets" // _next를 /assets로 변경
 };
 
 export default nextConfig;
